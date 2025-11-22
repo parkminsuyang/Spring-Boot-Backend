88af738f5ce09ea406670f7574b9806a7cfa6b72
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-22 22:10:28 +0900

    기본생성자(롬복) 추가

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -2,10 +2,12 @@ package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Article {
     @Id
